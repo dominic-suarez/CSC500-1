887af836ae448a87eeb38696ca48afac1ceabf32
dominic-suarez <160551207+dominic-suarez@users.noreply.github.com>
2024-02-20 12:44:50 -0700

    Add files via upload

---
+++ Critical_Thinking_Part2.py
@@ -0,0 +1,7 @@
+#Prompt user to input two numbers
+num1=int( input("Please enter the value of the first number = "))
+num2=int( input("Please enter the value of the second number = "))
+multiplication=num1*num2
+division=num1//num2
+print(multiplication)
+print(division)
