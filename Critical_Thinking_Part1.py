c32246ef785db7368d2b07531a2ba4435f45f38d
dominic-suarez <160551207+dominic-suarez@users.noreply.github.com>
2024-04-16 18:55:05 -0700

    Delete Critical_Thinking_Part1.py

--- Critical_Thinking_Part1.py
+++
@@ -1,8 +0,0 @@
-#Prompt user to input two numbers
-num1=int( input("Please enter the value of the first number = "))
-num2=int( input("Please enter the value of the second number = "))
-addition=num1+num2
-subtraction=num1-num2
-print(addition)
-print(subtraction)
-
